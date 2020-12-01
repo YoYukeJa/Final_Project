5ee7b3a22490b843e5edffbfd5ba1b102ba7ec4d
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-01 12:50:37 +0100

    Added and edited groupdb and IGroupdb

--- src/Database/GroupsDatabase.java
+++ src/Database/GroupsDatabase.java
@@ -1,4 +1,43 @@
 package Database;
 
-public class GroupsDatabase {
+import Model.User_related.Groups;
+import User_related.NormalGroup;
+import User_related.Users;
+
+import java.util.List;
+import java.util.Vector;
+
+public class GroupsDatabase extends Database implements IGroupsDatabase {
+    protected final Vector<Groups> groupDB;
+
+    public GroupsDatabase() {
+        this.groupDB = new Vector<>();
+    }
+
+    @Override
+    public void addEntry(String _name) {
+        int id = generateAuthenticId(groupDB);
+        Groups g = new NormalGroup(_name, id);
+        this.groupDB.add(g);
+    }
+
+    @Override
+    public void editGroupEntry(Groups g) {
+        groupDB.set(g.getIndex(), g);
+    }
+
+    @Override
+    public Groups getGroupEntry(int id) {
+        return groupDB.get(id);
+    }
+
+    @Override
+    public void addMemberToGroup(int id, Users u) {
+        groupDB.get(id).addMember(u.getUserId());
+    }
+
+    @Override
+    public List<Integer> getUsersFromGroup(int id){
+        return this.groupDB.get(id).getUsers();
+    }
 }
---
+++ src/Database/IGroupsDatabase.java
@@ -0,0 +1,13 @@
+package Database;
+
+import Model.User_related.Groups;
+import User_related.Users;
+
+import java.util.List;
+
+public interface IGroupsDatabase {
+    void editGroupEntry(Groups g);
+    Groups getGroupEntry(int id);
+    void addMemberToGroup(int id, Users u);
+    List<Integer> getUsersFromGroup(int id);
+}
