0a438c8304d08cfb699e6643027732c8b269eae4
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-08 13:27:34 +0100

    Update MainPage_GUI.java
    Added method to increment counter

--- src/View/MainPage_GUI.java
+++ src/View/MainPage_GUI.java
@@ -2,14 +2,29 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainPage_GUI {
+public class MainPage_GUI implements ActionListener {
+    private int count = 0;
+    private JFrame frame;
+    private JLabel label;
+    private JButton button;
+
 
     public MainPage_GUI(){
         JFrame frame = new JFrame();
+
+        button = new JButton("Click me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
+
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,5 +35,11 @@ public class MainPage_GUI {
 
     public static void main(String[] args){
         new MainPage_GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
     }
 }
