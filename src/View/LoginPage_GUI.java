435384bfea6c20e56a0b12b5be9f5d825d0bcc1e
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-10 14:52:48 +0100

    Added login screen
    Logic behind screen not yet implemented

--- src/View/LoginPage_GUI.java
+++ src/View/LoginPage_GUI.java
@@ -7,24 +7,42 @@ import java.awt.event.ActionListener;
 
 public class LoginPage_GUI extends JFrame implements ActionListener {
     private JFrame frame;
-    private JLabel label;
+    private JPanel panel;
+    private JLabel userLabel, passwordLabel, successLabel, failureLabel;
     private JButton button;
+    private JTextField userText;
+    private JPasswordField passwordText;
 
     public LoginPage_GUI(){
         frame = new JFrame();
+        panel = new JPanel();
+        userLabel = new JLabel("User");
+        passwordLabel = new JLabel("Password");
+        successLabel = new JLabel("");
+        button = new JButton("Login");
+        userText = new JTextField(20);
+        passwordText = new JPasswordField();
 
-        button = new JButton("Click me to go to the next page");
+        button.setBounds(10, 80, 80, 25);
         button.addActionListener(this);
-        button.setSize(30, 10);
 
-        label = new JLabel("loginpage");
+        userLabel.setBounds(10, 20, 80, 25);
+        passwordLabel.setBounds(10, 50, 80, 25);
+        successLabel.setBounds(10, 110, 300, 25);
 
-        JPanel panel = new JPanel();
+        userText.setBounds(100, 20, 165, 25);
+        passwordText.setBounds(100, 50, 165, 25);
+
         panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 200, 300));
-        panel.setLayout(new GridLayout(0, 1));
+        panel.setLayout(null);
         panel.add(button);
-        panel.add(label);
+        panel.add(userLabel);
+        panel.add(passwordLabel);
+        panel.add(successLabel);
+        panel.add(userText);
+        panel.add(passwordText);
 
+        frame.setSize(100, 100);
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Main Page");
@@ -38,8 +56,16 @@ public class LoginPage_GUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        MainPage_GUI mainPage_gui = new MainPage_GUI();
-        mainPage_gui.setVisible(true);
-        this.setVisible(false);
+        String user = userText.getText();
+        String password = passwordText.getText();
+
+        System.out.println(user + ", " + password);
+
+        if (user.equals("Anso") && password.equals("appelsap")){
+            successLabel.setText("Succesful login");
+        }
+        //MainPage_GUI mainPage_gui = new MainPage_GUI();
+        //mainPage_gui.setVisible(true);
+        //this.setVisible(false);
     }
 }
