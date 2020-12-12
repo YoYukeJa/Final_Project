a85ee08c82f2ec2dc29df0e2aff0fd136e91500f
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-12 14:42:35 +0100

    Update TicketFactory.java

--- src/Model/Factories/TicketFactory.java
+++ src/Model/Factories/TicketFactory.java
@@ -5,7 +5,7 @@ import User_related.Ticket;
 
 import java.util.List;
 
-public class TicketFactory implements TicketAbstractFactory {
+public class TicketFactory implements TicketAbstractFactory<Ticket> {
 
     @Override
     public Ticket create(int _id, int group_id, List<Integer> u_ids, List<Double> user_amounts, Boolean split_evenly, int k) {
