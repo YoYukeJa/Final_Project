130c17c4c384610809c04fb2685f9dabedfc3d95
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-12 14:42:41 +0100

    Update FactoryProvider.java

--- src/Model/Factories/FactoryProvider.java
+++ src/Model/Factories/FactoryProvider.java
@@ -1,11 +1,13 @@
 package Model.Factories;
 
 public class FactoryProvider {
-    public static AbstractFactory getFactory(String choice){//, String _name, int _id){
+    public static Object getFactory(String choice){//, String _name, int _id){
         if ("user".equalsIgnoreCase(choice)){
             return new UserFactory();
         } else if ("group".equalsIgnoreCase(choice)){
             return new GroupFactory();
+        } else if ("ticket".equalsIgnoreCase(choice)){
+            return new TicketFactory();
         }
         return null;
     }
