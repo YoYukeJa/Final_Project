48b021c31010cadde0f871d77a1d0855ff792c20
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-12 20:13:08 +0100

    Update MainPage_GUI.java

--- src/View/MainPage_GUI.java
+++ src/View/MainPage_GUI.java
@@ -5,26 +5,31 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainPage_GUI extends JFrame implements ActionListener {
-    private int count = 0;
+public class MainPage_GUI extends JFrame {
     private JFrame frame;
     private JLabel label;
-    private JButton button;
+    private JButton group_button, new_ticket_button, ticket_history_button;
 
 
     public MainPage_GUI(){
         frame = new JFrame();
 
-        button = new JButton("Click me");
-        button.addActionListener(this);
-        button.setSize(300, 100);
+        group_button = new JButton("Groups");
+        new_ticket_button = new JButton("Add ticket");
+        group_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        group_button.setSize(300, 100);
 
         label = new JLabel("Number of clicks: 0");
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(600, 600, 200, 600));
         panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
+        panel.add(group_button);
         panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
@@ -32,13 +37,5 @@ public class MainPage_GUI extends JFrame implements ActionListener {
         frame.setTitle("Main Page");
         frame.pack();
         frame.setVisible(true);
-    }
-
-
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks: " + count);
     }
 }
