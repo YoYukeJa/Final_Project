e9066122731a9403aa22f4518eb2e035e41ca505
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-12 14:42:29 +0100

    Update TicketAbstractFactory.java

--- src/Model/Factories/TicketAbstractFactory.java
+++ src/Model/Factories/TicketAbstractFactory.java
@@ -2,7 +2,7 @@ package Model.Factories;
 
 import java.util.List;
 
-public interface TicketAbstractFactory {
-    User_related.Ticket create(int _id, int group_id, List<Integer> u_ids, List<Double> user_amounts, Boolean split_evenly, int k);
+public interface TicketAbstractFactory<T> {
+    T create(int _id, int group_id, List<Integer> u_ids, List<Double> user_amounts, Boolean split_evenly, int k);
 
 }
