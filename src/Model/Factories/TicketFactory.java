c1b09ad83789b4638210e1164ec17687aabe0b4e
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-12 14:29:16 +0100

    Update TicketFactory.java
    implemented create method

--- src/Model/Factories/TicketFactory.java
+++ src/Model/Factories/TicketFactory.java
@@ -1,4 +1,14 @@
 package Model.Factories;
 
-public class TicketFactory extends FactoryProvider {
+import Model.User_related.NormalGroup;
+import User_related.Ticket;
+
+import java.util.List;
+
+public class TicketFactory implements TicketAbstractFactory {
+
+    @Override
+    public Ticket create(int _id, int group_id, List<Integer> u_ids, List<Double> user_amounts, Boolean split_evenly, int k) {
+        return new Ticket(_id, group_id, u_ids, user_amounts, split_evenly, k);
+    }
 }
