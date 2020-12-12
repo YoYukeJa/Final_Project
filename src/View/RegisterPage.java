5209609359cdcf36b8a27d4ca289b160953df5ba
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-12 12:13:15 +0100

    Create RegisterPage.java

---
+++ src/View/RegisterPage.java
@@ -0,0 +1,17 @@
+package View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegisterPage extends JFrame implements ActionListener {
+
+    public RegisterPage(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
