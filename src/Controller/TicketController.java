9c1cbb549cc34047ad892a82ff4fd3a1084904dc
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-13 18:34:39 +0100

    TicketGUI now can call for the group ids of the current user

--- src/Controller/TicketController.java
+++ src/Controller/TicketController.java
@@ -8,7 +8,16 @@ public class TicketController extends AbstractController{
 
     }
 
-    public boolean CreateNewTicket(Boolean split_evenly, List<Double> payment_amounts, int ticket_type, int group_id){
+    public String getGroupName(int id){
+        return groupsDB.getGroupEntry(id).getName();
+    }
+
+    public List<Integer> getGroupsFromUser(){
+        System.out.println(userDB.getUserEntry(current_user).getGroupIDList());
+        return userDB.getUserEntry(current_user).getGroupIDList();
+    }
+
+    public boolean createNewTicket(Boolean split_evenly, List<Double> payment_amounts, int ticket_type, int group_id){
         ticketDB.addTicketEntry(group_id, groupsDB.getUsersFromGroup(group_id), payment_amounts, split_evenly, ticket_type);
         return false;
     }
--- src/View/AddTicket_GUI.java
+++ src/View/AddTicket_GUI.java
@@ -1,12 +1,16 @@
 package View;
 
+import Controller.TicketController;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class AddTicket_GUI {
     private JFrame frame;
@@ -16,18 +20,29 @@ public class AddTicket_GUI {
     private List<JTextField> amount_per_user;
     private Boolean split_evenly;
     private JCheckBox splitting_evenly;
+    private TicketController ticketController;
+    private JComboBox type_options;
+
+    private int groupID;
 
 
     public AddTicket_GUI(){
     }
 
     public void Initialize() {
+        System.out.println();
+        ticketController = new TicketController();
         split_evenly = false;
-
+        List<Integer> groupids = ticketController.getGroupsFromUser();
+        List<String> group_names = new ArrayList<>();
+        for (int i = 0; i < groupids.size(); i++){
+            group_names.add(ticketController.getGroupName(groupids.get(i)));
+        }
         frame = new JFrame();
         add_ticket_button = new JButton("Add ticket");
         splitting_evenly = new JCheckBox("Splitting evenly");
         input_amount = new JTextField(20);
+        type_options = new JComboBox(group_names.toArray());
 
         splitting_evenly.setBounds(100, 100, 200, 50);
         add_ticket_button.setBounds(10, 120, 80, 25);
@@ -35,13 +50,16 @@ public class AddTicket_GUI {
         add_ticket_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                //ticketController.createNewTicket(split_evenly);
             }
         });
+
+
 
         splitting_evenly.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
+
                 split_evenly = !split_evenly;
                 System.out.println(split_evenly);
             }
@@ -55,6 +73,7 @@ public class AddTicket_GUI {
         panel.add(add_ticket_button);
         panel.add(splitting_evenly);
         panel.add(label);
+        panel.add(type_options);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
