80b6351ba6a1ea6dc938f54333879483c270d25e
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-21 13:10:00 +0100

    Update MainController.java
    getTicketHistory returns the tickets that are coupled to a User's account.

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -2,7 +2,17 @@ package Controller;
 
 import Model.User_related.Ticket;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainController extends AbstractController{
+
+    public List<Ticket> getTicketHistory(int id){
+        List<Ticket> tickets = new ArrayList<>();
+        for (int ticket_id: userDB.getUserEntry(id).getTickets()
+             ) {
+            tickets.add(ticketDB.getTicketEntry(ticket_id));
+        }
+        return tickets;
+    }
 }
