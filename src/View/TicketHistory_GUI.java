d3f271735c0271260ea8f1ef593882dfc6f8fddc
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-21 19:02:51 +0100

    Create TicketHistory_GUI.java

---
+++ src/View/TicketHistory_GUI.java
@@ -0,0 +1,20 @@
+package View;
+
+import Controller.MainController;
+import Model.User_related.Ticket;
+
+import javax.swing.*;
+import java.util.List;
+
+public class TicketHistory_GUI extends JFrame {
+    //private MainController mainController;
+    private JFrame frame;
+    private JLabel label;
+    private JButton close_button;
+
+    public TicketHistory_GUI(){}
+
+    public void Initialize(List<Ticket> tickets){
+
+    }
+}
