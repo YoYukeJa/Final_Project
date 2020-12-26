e2ec0138ee0dfaf653a4045b98322081c7fceff3
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-26 18:00:14 +0100

    removed TODO comment

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -9,7 +9,7 @@ public class LoginController extends AbstractController{
 
     public Boolean checkIfLoginIsCorrect(String _username, String _password){
         for (int i = 0; i < userDB.getLengthOfDatabase(); i++) {
-            if (userDB.getUserEntry(i).getName().equals(_username)){ //TODO: Password check nog toevoegen
+            if (userDB.getUserEntry(i).getName().equals(_username)){
                 if (userDB.getUserEntry(i).checkIfPasswordCorrect(_password.hashCode())) {
                     current_user = i;
                     return true;
