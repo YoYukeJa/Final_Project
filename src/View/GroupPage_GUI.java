a388e69ef6e18d056a97cfc7e7e7d0408610c4e1
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-27 18:28:50 +0100

    Variables in GroupPage_GUI get initialized

--- src/View/GroupPage_GUI.java
+++ src/View/GroupPage_GUI.java
@@ -1,4 +1,32 @@
 package View;
 
-public class GroupPage_GUI {
+import Controller.MainController;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupPage_GUI extends JFrame {
+
+    private GroupController controller;
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label;
+    private double total_amount;
+    private List<List<JLabel>> payments, group_member_names;
+    private JButton close_button;
+
+    public GroupPage_GUI(){
+
+    }
+
+    public void Initialize() {
+        controller = new GroupController();
+        frame = new JFrame();
+        close_button = new JButton("Close page");
+        panel = new JPanel();
+        label = new JLabel("User");
+        payments = new ArrayList<List<JLabel>>();
+        group_member_names = new ArrayList<List<JLabel>>();
+    }
 }
