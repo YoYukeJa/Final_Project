1c8c6f18497c153f4c3d5190b926f0bfde9a64d3
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-27 19:21:52 +0100

    implemented Groupcontroller methods for GUI interface

--- src/Controller/GroupController.java
+++ src/Controller/GroupController.java
@@ -1,4 +1,35 @@
 package Controller;
 
-public class GroupController {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupController extends AbstractController {
+    public GroupController(){
+
+    }
+
+    public List<String> getGroupMembers(int id){
+        List<Integer> member_ids = groupsDB.getUsersFromGroup(id);
+        List<String> member_names = new ArrayList<>();
+        for (Integer member: member_ids
+             ) {
+            member_names.add(userDB.getUserEntry(member).getName());
+        }
+        return member_names;
+    }
+
+    public List<Integer> getGroupsFromUser(){
+        System.out.println(userDB.getUserEntry(current_user).getGroupIDList());
+        return userDB.getUserEntry(current_user).getGroupIDList();
+    }
+
+    public Double getTotalAmountFromGroup(int id){
+        double total = 0.00;
+        List<Integer> tickets = groupsDB.getGroupEntry(id).getTicketIDList();
+        for (Integer ticket: tickets
+             ) {
+            total += ticketDB.getTicketEntry(ticket).getTotalAmount();
+        }
+        return total;
+    }
 }
