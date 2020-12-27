a53f4eb2f4a2e4f52c2cfd586b0b89426311fd53
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-27 20:45:33 +0100

    first version of grouppage GUI working

--- src/View/GroupPage_GUI.java
+++ src/View/GroupPage_GUI.java
@@ -4,6 +4,9 @@ import Controller.GroupController;
 import Controller.MainController;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,22 +15,70 @@ public class GroupPage_GUI extends JFrame {
     private GroupController controller;
     private JFrame frame;
     private JPanel panel;
-    private JLabel label;
+    private JLabel total_amount_label;
     private double total_amount;
-    private List<List<JLabel>> payments, group_member_names;
+    private List<Integer> group_ids;
+    private List<String> group_members, group_names;
+    private List<JLabel> group_member_names;
     private JButton close_button;
+    private JComboBox group_list;
+    private int current_group_id;
 
     public GroupPage_GUI(){
 
     }
 
     public void Initialize() {
+        current_group_id = 0;
         controller = new GroupController();
+        group_ids = controller.getGroupsFromUser();
         frame = new JFrame();
         close_button = new JButton("Close page");
         panel = new JPanel();
-        label = new JLabel("User");
-        payments = new ArrayList<List<JLabel>>();
-        group_member_names = new ArrayList<List<JLabel>>();
+        total_amount_label = new JLabel(controller.getTotalAmountFromGroup(current_group_id).toString());
+
+        group_names = controller.getGroupNamesFromCurrentUser();
+        group_list = new JComboBox(group_names.toArray());
+        group_list.setSize(80, 25);
+
+        group_member_names = new ArrayList<JLabel>();
+        group_members = controller.getGroupMembers(group_ids.get(current_group_id));
+        for (int i = 0; i < controller.getGroupMembers(current_group_id).size(); i++) {
+            group_member_names.add(new JLabel(group_members.get(i)));
+            group_member_names.get(i).setBounds(100, 20 + (i * 25), 80, 25);
+            panel.add(group_member_names.get(i));
+        }
+
+
+
+        group_list.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(group_list.getSelectedItem());
+                for (int i = 0; i < group_names.size(); i++) {
+                    if (group_list.getSelectedItem().equals(group_names.get(i))){
+                        current_group_id = group_ids.get(i);
+                    }
+                }
+            }
+        });
+
+        close_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                frame.dispose();
+            }
+        });
+
+        panel.add(group_list);
+        panel.add(close_button);
+        panel.add(total_amount_label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Groups");
+        frame.pack();
+        frame.setVisible(true);
     }
 }
