98472da0d4d2c505f403b156bdaea56f4cf1f05a
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-29 19:10:27 +0100

    Groups can now be added

--- src/View/AddGroupPage_GUI.java
+++ src/View/AddGroupPage_GUI.java
@@ -12,13 +12,14 @@ import java.util.List;
 
 public class AddGroupPage_GUI extends JFrame implements IDefaultPage_GUI{
     private GroupController controller;
-    private JFrame frame, pop_up_frame;
+    private JFrame frame, pop_up_frame, add_friend_frame;
     private JPanel panel;
     private JLabel label, name_label;
     private List<JLabel> username_labels;
     private JButton close_button, save_button, add_friend_button;
     private JTextField group_name;
     private JComboBox friend_list;
+    private List<String> selected_friends;
 
     public AddGroupPage_GUI(){
 
@@ -29,13 +30,16 @@ public class AddGroupPage_GUI extends JFrame implements IDefaultPage_GUI{
         controller = new GroupController();
         frame = new JFrame();
         pop_up_frame = new JFrame();
-        close_button = new JButton("Close page"); //TODO: Add pop-up for confirmation about closing without saving
+        add_friend_frame = new JFrame();
+        close_button = new JButton("Close page");
         save_button = new JButton("Create group");
         panel = new JPanel();
         label = new JLabel("Create a new group");
         name_label = new JLabel("Set the name of the group: ");
         group_name = new JTextField(100);
         username_labels = new ArrayList<>();
+        selected_friends = new ArrayList<>();
+
         friend_list = new JComboBox(controller.getFriendList().toArray());
         add_friend_button = new JButton("Add friend");
 
@@ -50,17 +54,22 @@ public class AddGroupPage_GUI extends JFrame implements IDefaultPage_GUI{
         pop_up_frame.setLayout(null);
         pop_up_frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
-        add_friend_button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+        add_friend_frame.setSize(300, 300);
+        add_friend_frame.setLayout(null);
+        add_friend_frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
         save_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 controller.createNewGroup(group_name.getText());
+                for (String friend_name: selected_friends
+                     ) {
+                    controller.addExistingGroupToUser(friend_name, group_name.getText());
+                }
+                MainPage_GUI mainPage_gui = new MainPage_GUI();
+                mainPage_gui.Initialize();
+                frame.setVisible(false);
+                frame.dispose();
             }
         });
 
@@ -71,16 +80,28 @@ public class AddGroupPage_GUI extends JFrame implements IDefaultPage_GUI{
                 if (answer==JOptionPane.YES_OPTION) {
                     frame.setVisible(false);
                     frame.dispose();
+                    pop_up_frame.setVisible(false);
+                    pop_up_frame.dispose();
                 }
+            }
+        });
+
+
+        add_friend_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selected_friends.add(friend_list.getSelectedItem().toString());
+                friend_list.removeItemAt(friend_list.getSelectedIndex());
             }
         });
 
         panel.add(save_button);
         panel.add(close_button);
-        panel.add(friend_list);
         panel.add(name_label);
         panel.add(group_name);
+        panel.add(friend_list);
         panel.add(add_friend_button);
+
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
