cae9987b48003e684aa5edc17fa7044dd753b7a4
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2020-12-29 19:10:10 +0100

    register page now sets correct current user id once registered

--- src/Controller/RegisterController.java
+++ src/Controller/RegisterController.java
@@ -9,6 +9,7 @@ public class RegisterController extends AbstractController {
     public Boolean RegisterNewUser(String _name, int _hash){
         if (!_name.isBlank()) {
             userDB.addEntry(_name, _hash);
+            current_user = userDB.getUserEntry(userDB.getLengthOfDatabase() - 1).getUserId();
             return true;
         }
         return false;
