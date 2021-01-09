46e2f1328c2a2c14fde32c1593572291b79e0071
mcbe1985 <45266247+mcbe1985@users.noreply.github.com>
2021-01-09 22:15:46 +0100

    small edits

--- src/Database/Interface/ITicketDatabase.java
+++ src/Database/Interface/ITicketDatabase.java
@@ -2,8 +2,11 @@ package Database.Interface;
 
 import Model.User_related.Ticket;
 
-public interface ITicketDatabase extends ITicketTypeDatabase {
+import java.util.List;
 
+public interface ITicketDatabase {
+
+    int addTicketEntry(int _group_id, List<Integer> u_ids, List<Double> _amount_per_user, boolean split_evenly, int k);
     void editTicketEntry(Ticket t);
     Ticket getTicketEntry(int id);
 }
--- src/Database/Interface/ITicketTypeDatabase.java
+++
@@ -1,9 +0,0 @@
-package Database.Interface;
-
-import Database.Database;
-
-import java.util.List;
-
-public interface ITicketTypeDatabase {
-    int addTicketEntry(int _group_id, List<Integer> u_ids, List<Double> _amount_per_user, boolean split_evenly, int k);
-}
